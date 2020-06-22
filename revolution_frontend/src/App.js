d84846b3b97f5e91ac8f803e134fb0d84c0753c6
Your Name <you@example.com>
2020-06-22 19:10:34 -0400

    feels good easy

--- revolution_frontend/src/App.js
+++ revolution_frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import MainDesktop from "./components/MainDesktop/MainDesktop";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Desktop</h1>
+        <MainDesktop />
       </header>
     </div>
   );
